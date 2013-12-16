255e06c75476bca3f0622cfadb2a432e6e51a01e
Erwin <erwin.van.tilburg@hotmail.com>
2013-12-16 14:12:00 +0100

    Toets1 commit1

---
+++ PyramidPanic/PyramidPanic/GameScene/QuitScene/QuitScene.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace PyramidPanic
+{
+    public class QuitScene : IState
+    {
+        //Fields
+        private PyramidPanic game;
+
+        public QuitScene(PyramidPanic game)
+        {
+            this.game = game;
+        }
+
+        public void Intialize()
+        {
+
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (Input.EdgeDetectKeyDown(Keys.B))
+            {
+                this.game.IState = this.game.StartScene;
+            }
+
+            if (Input.EdgeDetectKeyDown(Keys.Left) || Input.EdgeDetectMousePressRight())
+            {
+                this.game.IState = this.game.GameOverScene;
+            }
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            this.game.GraphicsDevice.Clear(Color.Red);
+
+
+        }
+    }
+}
+
